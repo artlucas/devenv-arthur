6b758e12a66203a76a321508f0aaf7ab8b73f74a
Art Lucas <artlucas@gmail.com>
2016-01-17 22:19:36 -0700

    added ansible config

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -7,4 +7,8 @@ sudo apt-get install -y sshpass \
   && sudo easy_install pip \
   && sudo pip install ansible
 
+sudo mkdir -p /etc/ansible
+
 sudo echo "localhost  ansible_connection=local" >> /etc/ansible/hosts
+
+sudo ansible-playbook -vv /vagrant/ansible/provision.yml
